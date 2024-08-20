b0d4ec856d4fca00e343622a084c29c77c147e48
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-21 04:39:38 +0500

    login page is finally working perfectly

---
+++ app/login/page.tsx
@@ -0,0 +1,12 @@
+import SignIn from '@/components/Auth/SignIn'
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div className=" p-3 mt-5 mx-auto">
+      <SignIn />
+    </div>
+  )
+}
+
+export default Login
