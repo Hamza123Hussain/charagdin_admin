c940664895849a3f97d5021fd7bf1854c4b64242
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-21 04:39:22 +0500

    layout for registered and unregistered  admin

---
+++ components/ConditonalLayout.tsx
@@ -0,0 +1,32 @@
+'use client'
+import React, { ReactNode, useContext, useEffect, useState } from 'react'
+
+import { usePathname } from 'next/navigation'
+import Login from '@/app/login/page'
+import { UserContext } from '@/utils/Context'
+
+const ConditionalLayout = ({ children }: { children: ReactNode }) => {
+  const { userData } = useContext(UserContext)
+  const [isClient, setIsClient] = useState(false)
+  const pathname = usePathname()
+
+  // Use useEffect to only set isClient on the client-side
+  useEffect(() => {
+    setIsClient(true)
+  }, [])
+
+  // Check if the current route is login or signup
+  const isAuthPage =
+    pathname === '/login' ||
+    pathname === '/signup' ||
+    pathname === '/forgotpass'
+
+  // Render the layout based on client-side state
+  if (!isClient) {
+    return null // or a loading spinner if preferred
+  }
+
+  return <>{userData || isAuthPage ? <> {children} </> : <Login />}</>
+}
+
+export default ConditionalLayout
