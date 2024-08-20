59db551f8fe74f1f1b00224b688f74038fe64c6c
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-21 04:38:45 +0500

    Navbar's functionality and development has been completed

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,14 +1,28 @@
+'use client'
+import { UserContext } from '@/utils/Context'
 import Image from 'next/image'
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import ShowingUser from './Auth/ShowingUser'
 
 const Navbar = () => {
+  const { userData } = useContext(UserContext)
+  const [isClient, setIsClient] = useState(false)
+
+  // Use useEffect to only set isClient on the client-side
+  useEffect(() => {
+    setIsClient(true)
+  }, [])
   return (
     <div className=" px-2 bg-slate-100 flex justify-between items-center p-1">
       <div className=" flex items-center">
         <Image src={'/Logo.png'} width={40} height={30} alt="Logo" />{' '}
         <h6 className=" text-lg text-slate-800 font-extrabold">CharagDin</h6>
       </div>
-      <h1 className=" text-lg text-slate-900 font-extralight">Admin Panel</h1>
+      {userData ? (
+        <ShowingUser />
+      ) : (
+        <h1 className=" text-lg text-slate-900 font-extralight">Admin Panel</h1>
+      )}
     </div>
   )
 }
