45313925d4484ad719c3072568a95f87c1764fbf
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-21 02:59:27 +0500

    Navbar Developed Completed

---
+++ components/Navbar.tsx
@@ -0,0 +1,16 @@
+import Image from 'next/image'
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className=" px-2 bg-slate-100 flex justify-between items-center p-1">
+      <div className=" flex items-center">
+        <Image src={'/Logo.png'} width={40} height={30} alt="Logo" />{' '}
+        <h6 className=" text-lg text-slate-800 font-extrabold">CharagDin</h6>
+      </div>
+      <h1 className=" text-lg text-slate-900 font-extralight">Admin Panel</h1>
+    </div>
+  )
+}
+
+export default Navbar
