56e47695d22576048d70a0b6d79cfd8759b96d7a
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-21 06:14:24 +0500

    functional component of creating catorgories

--- app/category/page.tsx
+++ app/category/page.tsx
@@ -1,7 +1,71 @@
-import React from 'react'
+'use client'
+import { CreateCatorgorie } from '@/functions/CRUD/Catorgories/Create'
+import { CreateProduct } from '@/functions/CRUD/Product/Create'
+import React, { useState } from 'react'
+const CreateCatogory = () => {
+  const [CatorgoryName, setCatorgoryName] = useState('')
+  const [imageFile, setImageFile] = useState(null)
+  const handleFileChange = (e: any) => {
+    setImageFile(e.target.files[0])
+  }
+  /**  CatorgoryName: string,
+  ImageUrl: string,
+  UserEmail: string,
+  Category: string */
+  const handleSubmit = async (e: any) => {
+    e.preventDefault()
+    const dATA = await CreateCatorgorie(CatorgoryName, 'hamza@gmail.com')
+  }
 
-const CreateCatorgories = () => {
-  return <div></div>
+  return (
+    <div className="flex justify-center items-center w-[50vw] mx-auto text-black  ">
+      <div className="  bg-white shadow-md rounded-lg p-8">
+        <h1 className="text-2xl font-semibold mb-6 text-gray-800">
+          Create Product
+        </h1>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-4">
+            <label
+              htmlFor="CatorgoryName"
+              className="block text-gray-700 text-sm font-medium mb-2"
+            >
+              Product Name
+            </label>
+            <input
+              type="text"
+              id="CatorgoryName"
+              value={CatorgoryName}
+              onChange={(e) => setCatorgoryName(e.target.value)}
+              className="w-full border border-gray-300 p-2 rounded-lg"
+              placeholder="Enter product name"
+              required
+            />
+          </div>
+          {/* <div className="mb-4">
+            <label
+              htmlFor="imageFile"
+              className="block text-gray-700 text-sm font-medium mb-2"
+            >
+              Image Upload
+            </label>
+            <input
+              type="file"
+              id="imageFile"
+              onChange={handleFileChange}
+              className="w-full border border-gray-300 p-2 rounded-lg"
+              required
+            />
+          </div> */}
+          <button
+            type="submit"
+            className="w-full bg-blue-500 text-white py-2 rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400"
+          >
+            Create Product
+          </button>
+        </form>
+      </div>
+    </div>
+  )
 }
 
-export default CreateCatorgories
+export default CreateCatogory
