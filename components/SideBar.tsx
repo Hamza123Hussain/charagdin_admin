f8cda621a632e3ea0193b5b11c2fd0dd5079011e
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-22 20:52:37 +0500

    fixed ui

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -18,7 +18,7 @@ const Sidebar = () => {
   const pathname = usePathname()
 
   return (
-    <div className="flex flex-col border-r-2 border-gray-200 text-gray-800 min-h-screen justify-between  bg-gray-100 py-4 shadow-md">
+    <div className="flex flex-col border-r-2 border-gray-200 text-gray-800 min-h-screen justify-normal lg:justify-between bg-gray-100 py-4 shadow-md">
       <div className=" flex flex-col gap-5">
         <Link href={'/'} className="flex items-center gap-2 px-4 mb-6">
           <Image src={'/Logo.png'} width={30} height={30} alt="Logo" />
